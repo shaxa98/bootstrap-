6c0c76139e2363107981d7fbcaea17412ab664d2
Shakhzod <96968478+shaxa98@users.noreply.github.com>
2023-04-12 13:02:55 +0500

    sss

--- second.js
+++ second.js
@@ -1,5 +1,22 @@
-const numbers = [1, 2, 3, 4, 5];
+// const numbers = [1, 2, 3, 4, 5];
 
-const filtered = numbers.filter((n) => n >= 0);
-const items = filtered.map((n) => "<li>" + n + "<li>");
-console.log(items);
+// const filtered = numbers.filter((n) => n >= 0);
+// const items = filtered.map((n) => "<li>" + n + "<li>");
+// console.log(items);
+
+const mainBanner = document.getElementById("main");
+console.log(mainBanner);
+
+const card = ['<div class="card">
+<img
+  src="https://static.tvmaze.com/uploads/images/medium_portrait/424/1061886.jpg"
+  class="card-img-top"
+  alt="..."
+/>
+<div class="card-body">
+  <h5 class="card-title">FBI</h5>
+  <p class="card-text">Obligation</p>
+</div>
+</div>'];
+
+mainBanner.push(card);
